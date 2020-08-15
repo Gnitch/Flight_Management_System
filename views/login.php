960f329eb3363a8a985d1b1e494db407aab65e16
Gnitch <sujoydcunha@gmail.com>
2020-08-15 19:49:43 +0530

    update sign-in and sign-up ui

---
+++ views/login.php
@@ -0,0 +1,37 @@
+<?php include_once '../helpers/helper.php'; ?>
+
+<?php subview('header.php'); ?>
+<link rel="stylesheet" href="../assets/css/login.css">
+<div class="flex-container">
+    <div class="login-form" style="height: 400px;">
+        <h1>Sign in</h1>
+        <form method="POST" action="../includes/login.inc.php">
+            <div class="flex-container">             
+                <div>
+                    <i class="fa fa-user"></i>
+                </div>
+                <div>
+                    <input type="text" name="user_id" placeholder="username or email-id" 
+                        class="form-input" required>
+                </div>
+            </div>
+            <div class="flex-container">
+                <div>
+                    <i class="fa fa-lock"></i>
+                </div>
+                <div>
+                    <input type="password" name="user_pass" class="form-input" 
+                        placeholder="password" required>
+                </div>
+            </div>
+            <a id="reset-pass" href="">reset password</a>
+            <div class="submit">
+            <button name="login_but" type="submit" class="button">Submit</button>                    
+            </div>
+        </form>            
+        <p class="text">Don't have a account? register <a href="register.php">here</a></p>                
+    </div>
+</div>
+
+<?php subview('footer.php'); ?> 
+
